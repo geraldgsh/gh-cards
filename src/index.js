378a25835ffaf9de72ad058cf09ade83abec666c
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 06:23:08 +0800

    third commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className = "github-profile">
+      <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+      </div>
+    </div>
+    );
+  }
+}
+
+class APP extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <APP title="The GitHub Cards App" />
   </React.StrictMode>,
   document.getElementById('root')
 );
