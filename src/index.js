b37480e9d7ab6e127e8b5e2c267a8af54a9b85a7
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 06:30:45 +0800

    added mock data

--- src/index.js
+++ src/index.js
@@ -3,34 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+const CardList = (props) => (
+  <div>
+    {testData.map(profile => <Card {...profile}/>)}
+  </div>
+);
+
 class Card extends React.Component {
   render() {
-    return (
-      <div className = "github-profile">
-      <img src="https://placehold.it/75" />
-        <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+    const profile = this.props;
+      return (
+        <div className="github-profile">
+          <img src={profile.avatar_url} />
+          <div className="info">
+            <div className="name">{profile.name}</div>
+            <div className="company">{profile.company}</div>
+        </div>
       </div>
-    </div>
     );
   }
 }
 
-class APP extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
-    )
-  }
+    );
+  } 
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <APP title="The GitHub Cards App" />
+    <App title="The GitHub Cards App" />
   </React.StrictMode>,
   document.getElementById('root')
 );
